d4f80259d8898bb6f3dcd9705cf2629a9f858bf7
lvchien <v.chienle@gmail.com>
2024-03-14 12:42:14 +0100

    Refactor symmetrized cfie files

--- examples/sym_cfie_cube.jl
+++ examples/sym_cfie_cube.jl
@@ -1,129 +1,91 @@
-using BEAST, CompScienceMeshes, LinearAlgebra, ConvolutionOperators, Plots, Printf
+using BEAST, CompScienceMeshes, LinearAlgebra, ConvolutionOperators, Plots, Printf, IterativeSolvers
 include("utils/genmesh.jl")
 
-# for meshsize in [0.055, 0.05, 0.045, 0.04]
-# Computational mesh
-meshsize = 0.1
+κ = π/2
+γ = -im*κ
+κ′ = κ
+η = -κ^2
+
+```
+    FOR CONDITION NUMBER W.R.T. h CALCULATION
+```
+### Different mesh sizes h for examining the condition number w.r.t. h
+# for meshsize in [0.35, 0.25, 0.18, 0.12, 0.09, 0.07, 0.055, 0.004]
+```
+    end
+```
+
+### Computational mesh
+meshsize = 0.18
 Γ = meshcuboid(1.0, 1.0, 1.0, meshsize)
 
-# RWG and BC function spaces
+### RWG and BC function spaces
 X = raviartthomas(Γ)
 Y = buffachristiansen(Γ)
-    																		
+                                                                            
 N = NCross()
 
-# Gram matrix
+### Gram matrix
 Nyx = assemble(N, Y, X)
 Nyy = assemble(N, Y, Y)
 iNyx = inv(Matrix(Nyx))
 iNxy = -transpose(iNyx)
 
-length_arr = 51
-cond_cfie, cond_efie = zeros(length_arr), zeros(length_arr)
-step = (√3 - √2)π/32
-κ_arr = √3π .+ step * [9:1:12;]
+```
+    FOR CONDITION NUMBER W.R.T. κ CALCULATION
+```
+### Different wave numbers κ for examining the condition number w.r.t. κ
+#     length_arr = 51
+#     step = (√3 - √2)π/32
+#     κ_arr = √3π .+ step * [-39:1:11;]
 
-# for i in 1:4
-    κ = √3π
-    # κ = κ_arr[i]
-    γ = -im*κ
-    η = κ^2
+# for κ ∈ κ_arr
+#     γ = -im*κ
+#     κ′ = κ
+#     η = -κ^2
+```
+    end
+```
 
-    # Operators
-    S = MWSingleLayer3D(γ, 1.0, -1.0/κ^2)                                      # EFIO with wave number κ 
-    K = MWDoubleLayer3D(γ)                                                     # MFIO with wave number κ
-    Si = MWSingleLayer3D(κ, 1.0, 1.0/κ^2)                                      # EFIO with pure imaginary wave number iκ 
-    Ki = MWDoubleLayer3D(κ)                                                    # MFIO with pure imaginary wave number iκ
-
-    # assembly of static operators
-    nearstrat = BEAST.DoubleNumWiltonSauterQStrat(6, 7, 6, 7, 9, 9, 9, 9)
-
-    ℤ = assemble(S, X, X, quadstrat=nearstrat)
-    𝕊x = assemble(Si, X, X, quadstrat=nearstrat) 
-    𝕊y = assemble(Si, Y, Y, quadstrat=nearstrat) 
-    𝕂x = assemble(K, X, X, quadstrat=nearstrat)
-    𝕂ix = assemble(Ki, X, X, quadstrat=nearstrat)
-    𝕂iy = assemble(Ki, Y, Y, quadstrat=nearstrat)
-
-    lhs = im*η * iNxy * ℤ * iNyx * 𝕊y + κ^2 * iNxy * 𝕊x * iNyx * 𝕊y +  iNxy * (𝕂x - 𝕂ix) * iNyx * (0.5*Nyy + 𝕂iy)
-    cond(lhs)
-
-    open("cube_kappa.txt", "a") do io
-        @printf(io, "%.3f %.10f %.10f\n", κ, cond(ℤ), cond(lhs))
-    end;
-# end
-
-
-
-
-# Exact solution
-
-using BEAST, CompScienceMeshes, LinearAlgebra, ConvolutionOperators, Plots, Printf, StaticArrays
-include("genmesh.jl")
-
-# Computational mesh
-meshsize = 0.1
-# Γ = meshsphere(1.0, 0.15)
-# Γ = meshcuboid(1.0, 1.0, 1.0, meshsize)
-
-# RWG and BC function spaces
-X = raviartthomas(Γ)
-Y = buffachristiansen(Γ)
-    
-N = NCross()
-
-# Gram matrix
-Nyx = assemble(N, Y, X)
-Nxx = assemble(N, X, X)
-Nyy = assemble(N, Y, Y)
-iNyx = inv(Matrix(Nyx))
-iNxy = -transpose(iNyx)
-
-κ = π/2                                                                 
-γ = -im*κ
-η = κ^2
-
-# Operators
+### Operators
 S = MWSingleLayer3D(γ, 1.0, -1.0/κ^2)                                      # EFIO with wave number κ 
 K = MWDoubleLayer3D(γ)                                                     # MFIO with wave number κ
-Si = MWSingleLayer3D(κ, 1.0, 1.0/κ^2)                                      # EFIO with pure imaginary wave number iκ 
-Ki = MWDoubleLayer3D(κ)                                                    # MFIO with pure imaginary wave number iκ
+Si = MWSingleLayer3D(κ′, 1.0, 1.0/κ′^2)                                      # EFIO with pure imaginary wave number iκ 
+Ki = MWDoubleLayer3D(κ′)                                                    # MFIO with pure imaginary wave number iκ
 
-# assembly of static operators
+E = Maxwell3D.planewave(direction=-ẑ, polarization=x̂, wavenumber=κ)
+e = (n × E) × n
+
+### Assembly of static operators
 nearstrat = BEAST.DoubleNumWiltonSauterQStrat(6, 7, 6, 7, 9, 9, 9, 9)
 
-ℤ = assemble(S, Y, Y, quadstrat=nearstrat)
-𝕊x = assemble(Si, Y, Y, quadstrat=nearstrat) 
-𝕊y = assemble(Si, X, X, quadstrat=nearstrat) 
-𝕂x = assemble(K, Y, Y, quadstrat=nearstrat)
-𝕂ix = assemble(Ki, Y, Y, quadstrat=nearstrat)
-𝕂iy = assemble(Ki, X, X, quadstrat=nearstrat)
+ℤ = assemble(S, X, X, quadstrat=nearstrat)
+𝕊x = assemble(Si, X, X, quadstrat=nearstrat) 
+𝕊y = assemble(Si, Y, Y, quadstrat=nearstrat) 
+𝕂x = assemble(K, X, X, quadstrat=nearstrat)
+𝕂ix = assemble(Ki, X, X, quadstrat=nearstrat)
+𝕂iy = assemble(Ki, Y, Y, quadstrat=nearstrat)
 
-lhs = im*η * iNyx * ℤ * iNxy * 𝕊y + κ^2 * iNyx * 𝕊x * iNxy * 𝕊y +  iNyx * (𝕂x - 𝕂ix) * iNxy * (0.5*Nxx + 𝕂iy)
-
-# Exact solution
-function exct(x)
-    r = norm(x)
-    expn = exp(-im*κ*r)
-    (-κ^2*r^2 + im*κ*r + 1)*expn/r^3*SVector(1.0, 0.0, 0.0) + (κ^2*r^2 - 3*im*κ*r - 3)*expn/r^5*x[1]*x
-end
-e = (n × exct) × n
+lhs = im*η * iNxy * ℤ * iNyx * 𝕊y + κ′^2 * iNxy * 𝕊x * iNyx * 𝕊y +  iNxy * (𝕂x - 𝕂ix) * iNyx * (0.5*Nyy + 𝕂iy)
 
 @hilbertspace k
-rhs = assemble(@discretise e[k] k∈Y)
+rhs = assemble(@discretise -e[k] k∈X)
 
-xi, = solve(BEAST.GMRESSolver(lhs, tol=1e-12, restart=250), iNyx * rhs)
 
-jc, mc = iNxy * 𝕊y * xi, iNxy * (0.5*Nxx + 𝕂iy) * xi
+### Solving cfie and efie by gmres (calculate iteration counts)
+xi, ch = IterativeSolvers.gmres(lhs, iNxy * rhs, log=true, maxiter=100000000, abstol=1e-8, reltol=1e-8)
+# xi2, ch2 = IterativeSolvers.gmres(im*κ*ℤ, rhs, log=true, maxiter=100000000, abstol=1e-8, reltol=1e-8)
 
-# Result visualization 
-Θ = range(0, stop=2π, length=100)
-pts = [SVector(2.0*cos(θ), 0.0, 2.0*sin(θ)) for θ in Θ]
-Esct = im*η * potential(MWSingleLayerField3D(γ, 1.0, -1.0/κ^2), pts, jc, Y) + potential(BEAST.MWDoubleLayerField3D(γ), pts, mc, Y)
-# Hsct = -1/(im*κ) * (im*η * potential(BEAST.MWDoubleLayerField3D(γ), pts, jc, X) + κ^2 * potential(MWSingleLayerField3D(γ, 1.0, -1.0/κ^2), pts, mc, X))
 
-Θ1 = range(0, stop=2π, length=100)
-pts1 = [SVector(2.0*cos(θ), 0.0, 2.0*sin(θ)) for θ in Θ1]
-Eexct = exct.(pts)
-# Hmie = scatteredfield(sp, ex, MagneticField(pts1))
+### Print the condition number and iteration counts w.r.t. κ to file
+# open("cube_kappa.txt", "a") do io
+    # @printf(io, "%.3f %.3f %d %.3f %d\n", κ, cond(lhs), ch.iters, cond(im*κ*ℤ), ch2.iters)
+# end;
 
+
+### Print the condition number and iteration counts w.r.t. h to file
+# open("cube_h.txt", "a") do io
+#     @printf(io, "%.3f %.3f %d %.3f %d\n", meshsize, cond(lhs), ch.iters, cond(im*κ*ℤ), ch2.iters)
+# end;
+
+# end
---
+++ examples/sym_cfie_sphere.jl
@@ -0,0 +1,153 @@
+using BEAST, CompScienceMeshes, LinearAlgebra, ConvolutionOperators, Plots, Printf, SphericalScattering, StaticArrays, IterativeSolvers
+include("utils/genmesh.jl")
+
+### Parameters
+κ = 1.43π                                                   # resonant wave number of the unit sphere
+# κ = 2π/3                                                  # non-resonnat wave number               
+γ = -im*κ
+κ′ = κ
+η = κ^2
+
+```
+    FOR ERROR CALCULATION ONLY
+```
+
+### Setting for SphericalScattering package
+# sp = PECSphere( 
+#     radius      = 1.0, 
+#     embedding   = Medium(1.0, 1.0),
+# )
+
+# ex = planeWave(
+#     embedding    = Medium(1.0, 1.0),
+#     frequency    = κ/(2π),
+# )
+
+### Evaluation points on the external sphere of radius r = 2m 
+# r = 2.0
+# Θ, φ = range(0, stop=π, length=50), range(0, stop=2π, length=101)
+# pts = [SVector(r*sin(θ)*cos(ϕ), r*sin(θ)*sin(ϕ), r*cos(θ)) for θ ∈ Θ for ϕ ∈ φ[1:end-1]]
+
+### Exact solutions by Mie series
+# Emie = scatteredfield(sp, ex, ElectricField(pts))
+# Hmie = scatteredfield(sp, ex, MagneticField(pts))
+
+```
+    end
+```
+
+
+```
+    FOR CONDITION NUMBER W.R.T. h CALCULATION
+```
+### Different mesh sizes h for examining the condition number w.r.t. h
+# for meshsize in [0.45, 0.35, 0.3, 0.25, 0.2, 0.18, 0.16, 0.15, 0.13, 0.12, 0.11, 0.1, 0.09, 0.075, 0.065, 0.0055]
+```
+    end
+```
+
+### Computational mesh
+meshsize = 0.15
+Γ = meshsphere(1.0, meshsize)
+
+### RWG and BC function spaces
+X = raviartthomas(Γ)
+Y = buffachristiansen(Γ)
+    
+N = NCross()
+
+### Gram matrix
+Nyx = assemble(N, Y, X)
+Nyy = assemble(N, Y, Y)
+iNyx = inv(Matrix(Nyx))
+iNxy = -transpose(iNyx)
+
+
+```
+    FOR CONDITION NUMBER W.R.T. κ CALCULATION
+```
+### Different wave numbers κ for examining the condition number w.r.t. κ
+#     length_arr = 51
+#     step = π/150
+#     κ_arr = 4π/3 .+ step * [0:1:50;]
+
+# for κ ∈ κ_arr[14:end]
+#     γ = -im*κ
+#     κ′ = κ
+#     η = -κ^2
+```
+    end
+```
+
+### Operators
+S = MWSingleLayer3D(γ, 1.0, -1.0/κ^2)                                      # EFIO with wave number κ 
+K = MWDoubleLayer3D(γ)                                                     # MFIO with wave number κ
+Si = MWSingleLayer3D(κ′, 1.0, 1.0/κ′^2)                                    # EFIO with pure imaginary wave number iκ′
+Ki = MWDoubleLayer3D(κ′)                                                   # MFIO with pure imaginary wave number iκ′
+
+E = Maxwell3D.planewave(direction=-ẑ, polarization=x̂, wavenumber=κ)
+e = (n × E) × n
+
+### Assembly of static operators
+nearstrat = BEAST.DoubleNumWiltonSauterQStrat(6, 7, 6, 7, 9, 9, 9, 9)
+
+ℤ = assemble(S, X, X, quadstrat=nearstrat)
+𝕊x = assemble(Si, X, X, quadstrat=nearstrat) 
+𝕊y = assemble(Si, Y, Y, quadstrat=nearstrat) 
+𝕂x = assemble(K, X, X, quadstrat=nearstrat)
+𝕂ix = assemble(Ki, X, X, quadstrat=nearstrat)
+𝕂iy = assemble(Ki, Y, Y, quadstrat=nearstrat)
+
+lhs = im*η * iNxy * ℤ * iNyx * 𝕊y + κ′^2 * iNxy * 𝕊x * iNyx * 𝕊y + iNxy * (𝕂x - 𝕂ix) * iNyx * (0.5*Nyy + 𝕂iy)
+
+@hilbertspace k
+rhs = assemble(@discretise -e[k] k∈X)
+
+xi, ch = IterativeSolvers.gmres(lhs, iNxy * rhs, log=true, abstol=1e-12, reltol=1e-12)
+
+jc, mc = iNyx * 𝕊y * xi, iNyx * (0.5*Nyy + 𝕂iy) * xi
+    
+
+```
+    FOR ERROR CALCULATION ONLY
+```
+# Esct = im*η*potential(MWSingleLayerField3D(γ, 1.0, -1.0/κ^2), pts, jc, X) + potential(BEAST.MWDoubleLayerField3D(γ), pts, mc, X)
+# curlEsct = im*η*potential(BEAST.MWDoubleLayerField3D(γ), pts, jc, X) + κ^2 * potential(MWSingleLayerField3D(γ, 1.0, -1.0/κ^2), pts, mc, X)
+
+# δe = Esct - conj.(Emie)
+# δcurle = curlEsct - (im*κ).*conj.(Hmie)
+# errorL2e = sqrt(real(dot(δe, δe)))/size(Emie)[1]
+# errorHcurle = sqrt(real(dot(δe, δe)) + real(dot(δcurle, δcurle)))/size(Emie)[1]
+```
+    end
+```
+
+### Print the error to file
+# open("sym_cfie_sphere_error_h_kappa.txt", "a") do io
+#     @printf(io, "%.3f %.10f %.3f %d %.10f %.10f \n", meshsize, κ′, cond(lhs), ch.iters, errorL2e, errorHcurle)
+# end; 
+
+
+### Print the condition number w.r.t. η to file
+# open("sym_cfie_sphere_cond_eta.txt", "a") do io
+#     @printf(io, "%.10f %.3f %.3f\n", η, cond(lhs_pos), cond(lhs_neg))
+# end; 
+
+
+### Solving cfie and efie by gmres (calculate iteration counts)
+# xi, ch = IterativeSolvers.gmres(lhs, iNxy * rhs, log=true, maxiter=100000000, abstol=1e-8, reltol=1e-8)
+# xi2, ch2 = IterativeSolvers.gmres(im*κ*ℤ, rhs, log=true, maxiter=100000000, abstol=1e-8, reltol=1e-8)
+
+
+### Print the condition number and iteration counts w.r.t. h to file
+# open("sym_cfie_sphere_cond_h.txt", "a") do io
+#     @printf(io, "%.3f %.3f %d %.3f %d \n", meshsize, cond(lhs), ch.iters, cond(im*κ*ℤ), ch2.iters)
+# end; 
+
+
+### Print the condition number and iteration counts w.r.t. κ to file
+# open("sphere_kappa.txt", "a") do io
+#     @printf(io, "%.3f %.3f %d %.3f %d \n", κ, cond(lhs), ch.iters, cond(im*κ*ℤ), ch2.iters)
+# end; 
+
+# end
