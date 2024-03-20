313cf1ab5ed9cb6df74d790ce50c1ee5b6e8ccc6
lvchien <v.chienle@gmail.com>
2024-03-20 17:16:01 +0100

    Add square-based pyramid

--- examples/utils/genmesh.jl
+++ examples/utils/genmesh.jl
@@ -673,3 +673,56 @@ function meshcone(radius, height, h)
     rm(fno)
     return m
 end
+
+
+
+"""
+    meshsqpyramid(width, height, h)
+
+Create a mesh of an square-based pyramid of `width` and `height`
+
+The target edge size is `h`.
+"""
+function meshsqpyramid(width, height, h)
+    fno = tempname() * ".msh"
+
+    gmsh.initialize()
+    gmsh.model.add("sqpyramid")
+    gmsh.model.geo.addPoint(0.0, 0.0, height, h, 1)
+    gmsh.model.geo.addPoint(width/2, -width/2, 0.0, h, 2)
+    gmsh.model.geo.addPoint(width/2, width/2, 0.0, h, 3)
+    gmsh.model.geo.addPoint(-width/2, width/2, 0.0, h, 4)
+    gmsh.model.geo.addPoint(-width/2, -width/2, 0.0, h, 5)
+
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(1, 3, 2)
+    gmsh.model.geo.addLine(1, 4, 3)
+    gmsh.model.geo.addLine(1, 5, 4)
+
+    gmsh.model.geo.addLine(2, 3, 5)
+    gmsh.model.geo.addLine(3, 4, 6)
+    gmsh.model.geo.addLine(4, 5, 7)
+    gmsh.model.geo.addLine(5, 2, 8)
+
+    gmsh.model.geo.addCurveLoop([-5, -6, -7, -8], 1)    
+    gmsh.model.geo.addPlaneSurface([1], 1)
+    gmsh.model.geo.addCurveLoop([1, -2, 5], 2)    
+    gmsh.model.geo.addPlaneSurface([2], 2)
+    gmsh.model.geo.addCurveLoop([2, -3, 6], 3)    
+    gmsh.model.geo.addPlaneSurface([3], 3)
+    gmsh.model.geo.addCurveLoop([3, -4, 7], 4)    
+    gmsh.model.geo.addPlaneSurface([4], 4)
+    gmsh.model.geo.addCurveLoop([4, -1, 8], 5)    
+    gmsh.model.geo.addPlaneSurface([5], 5)
+
+    gmsh.model.geo.synchronize()
+    gmsh.option.setNumber("Mesh.MshFileVersion",2)
+    gmsh.model.mesh.generate(2)
+    # gmsh.fltk.run()
+    gmsh.write(fno)
+    gmsh.finalize()
+
+    m = CompScienceMeshes.read_gmsh_mesh(fno)
+    rm(fno)
+    return m
+end
