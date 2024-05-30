c4b95f552994d4fc43eeab4fb5d9e36606004c96
lvchien <v.chienle@gmail.com>
2024-05-30 16:25:32 +0200

    plot

--- examples/utils/plot.jl
+++ examples/utils/plot.jl
@@ -1,9 +1,8 @@
-using Plots, DelimitedFiles, Plotly
+import Plots, DelimitedFiles, Plotly
 
-
-# Server 31: torus with dt = 0.1, Nt=3600, h = 0.6m
-# Server 32: sphere 
-
+# Server 31: 
+# Server 32: 
+# Server 33: cuboid
 
 ```
     Plot the triangle mesh
@@ -12,63 +11,70 @@ using Plots, DelimitedFiles, Plotly
 using PlotlyJS
 
 pt1 = PlotlyJS.plot(
-    [patch(Γ, opacity=1.0, color="#c0c0c4"), CompScienceMeshes.wireframe(Γ)],
+    [patch(Γ, opacity=1.0, color="#bcbcbc"), CompScienceMeshes.wireframe(Γ), 
+    CompScienceMeshes.normals(Γ)],
     Layout(
-        height=400, width=400,
+        height=400, width=600,
         scene=attr(
             xaxis=attr(
-                color="#FFFFFF",
+                visible=false,
                 showbackground=false
             ),
             yaxis=attr(
-                color="#FFFFFF",
+                visible=false,
                 showbackground=false
             ),
             zaxis=attr(
-                color="#FFFFFF",
+                visible=false,
                 showbackground=false
+            ),
+            camera=attr(
+                up=attr(x=0, y=0, z=1),
+                center=attr(x=0, y=0, z=0),
+                eye=attr(x=0.8, y=1, z=1)
             )
         )
     )
 )
 
-
-
+savefig(pt1, "starpyramid.pdf", width=600, height=400, scale=8)
 
 
 ```
     Plot the current density
 ```
 
-x = 10 * Δt/3 * [1:1:Nt;]
+x = 1e-2 * Δt/3 * [1:1:Nt;]
 
+# Plots.plotly()
 plt = Plots.plot(
     size = (600, 400),
     grid = false,
     xscale = :identity, 
-    xlims = (0, 400),
-    # xticks = [450, 900, 1350],
-    xtickfont = font(10, "Computer Modern"), 
+    # xlims = (0, 16),
+    # xticks = [0, 1, 2, 3, 4],
+    xtickfont = Plots.font(10, "Computer Modern"), 
     yscale = :log10, 
-    ylims = (1e-30, 1e0), 
-    yticks = [1e-35, 1e-30, 1e-25, 1e-20, 1e-15, 1e-10, 1e-5, 1e0, 1e5],
-    ytickfont = font(10, "Computer Modern"),
-    xlabel = "Time (ns)",
+    ylims = (1e-20, 1e0), 
+    yticks = [1e-50, 1e-45, 1e-40, 1e-35, 1e-30, 1e-25, 1e-20, 1e-15, 1e-10, 1e-5, 1e0, 1e5],
+    ytickfont = Plots.font(10, "Computer Modern"),
+    xlabel = "Time ({\\mu}s)",
     ylabel = "Current density intensity (A/m)",
-    titlefont = font(10, "Computer Modern"),
-    guidefont = font(11, "Computer Modern"),
-    colorbar_titlefont = font(10, "Computer Modern"),
-    legendfont = font(11, "Computer Modern"),
-    legend = :topright)
+    titlefont = Plots.font(10, "Computer Modern"),
+    guidefont = Plots.font(11, "Computer Modern"),
+    colorbar_titlefont = Plots.font(10, "Computer Modern"),
+    legendfont = Plots.font(11, "Computer Modern"),
+    legend = :left,
+    dpi = 300)
 
-Plots.plot!(x, abs.(je[1, :]), label="TD-EFIE", linecolor=1, lw=1.3)
-# Plots.plot!(x[1:1200], abs.(j3[1, 1:1200]), label="qHP CP TD-EFIE", linecolor=2)
-Plots.plot!(x, abs.(jm[1, :]), label="", linecolor=1, lw=1.3)
-# Plots.plot!(x[1:1200], abs.(j5[1, 1:1200]), label="qHP TD-MFIE", linecolor=4)
-Plots.plot!(x, abs.(jc[1, :]), label="mixed TD-CFIE", linecolor=3)
-Plots.plot!(x, abs.(jqhpc[1, :]), label="TD-CFIE", linecolor=3, lw=1.4)
+Plots.plot!(x, abs.(je[1, :]), label="Exact solution", linecolor=2, lw=1.5)
+# Plots.plot!(x, abs.(jex4[1, :]), label="qHP TD-CFIE", linecolor=4, lw=1.3)
+Plots.plot!(x, abs.(jpmchwt13[1, :]), label="TD-EFIE", linecolor=1, lw=1.5)
+Plots.plot!(x, abs.(xefie_irk[1, :]), label="TD-MFIE", linecolor=4, lw=1.5)
+Plots.plot!(x, abs.(jc[1, :]), label="TD-CFIE", linecolor=4, lw=1.5)
+Plots.plot!(x, abs.(jqhp[1, :]), label="qHP PMCHWT", linecolor=3, lw=1.5)
 
-Plots.savefig("static.pdf")
+Plots.savefig("current_sqtorus_cdt_1.5_h_0.3m.pdf")
 
 
 ```
@@ -80,37 +86,64 @@ wm = ConvolutionOperators.polyvals(Kyx)
 wc = ConvolutionOperators.polyvals(cfie)
 wsc = ConvolutionOperators.polyvals(qhpcfie)
 
-pe = Plots.plot(exp.(im*range(0,2pi,length=1000)), label="")
-scatter!(we, label="")
-pm = Plots.plot(exp.(im*range(0,2pi,length=1000)), label="")
-scatter!(wm, label="")
-pc = Plots.plot(width = 400, height=400)
-plot!(exp.(im*range(0,2pi,length=1000)), label="")
-scatter!(wc, label="")
-psc = Plots.plot(exp.(im*range(0,2pi,length=1000)), label="")
-scatter!(wsc, label="")
-plot(size=(600,600),
+Plots.plotly()
+Plots.plot(size=(400,400),
+    exp.(im*range(0, 2pi,length=3000)),
+    aspect_ratio = :equal,
+    xlims=(-1.2, 1.2),
+    ylims=(-1.2, 1.2),
     grid = false,
-    pe, pm, pc, psc, 
-    layout = 4, 
-    markercolor=:black,
-    markersize=2.5,
     linecolor=:red,
-    xlabel = "Real part",
+    lw=1.5,
+    label="diameter = 8, c.dt = 3",
+    xlabel="",
+    ylabel="")
+
+Plots.scatter!(wm,
+    label="", 
+    markercolor=:blue, 
+    markersize=3.5, 
+    xlabel = "Real part", 
     ylabel = "Imaginary part",
-    title=["(a) standard TD-EFIE" "(b) standard TD-MFIE" "(c) mixed TD-CFIE" "(d) symmetrized TD-CFIE"],
-    xtickfont = font(9, "Computer Modern"), 
-    ytickfont = font(9, "Computer Modern"), 
-    titlefont = font(11, "Computer Modern"),
-    guidefont = font(9, "Computer Modern"),
-    colorbar_titlefont = font(10, "Computer Modern"),
-    legendfont = font(9, "Computer Modern"))
+    xtickfont = Plots.font(10, "Computer Modern"), 
+    ytickfont = Plots.font(10, "Computer Modern"), 
+    titlefont = Plots.font(12, "Computer Modern"),
+    guidefont = Plots.font(10, "Times"),
+    colorbar_titlefont = Plots.font(11, "Times"),
+    legendfont = Plots.font(10, "Computer Modern"),
+    dpi = 600)
 
-savefig("polyvals_sphere_h_0.3m_cdt_0.25m.pdf")
+Plots.savefig("polyvals_sqtorus_qHP_PMCHWT_13_h_0.4m_cdt_1.5m.pdf")
 
+# Polyvals with small window for zoom-in data
+Plots.plot!(
+    exp.(im*range(-pi/5,pi/5,length=10000)), 
+    xlims = (0.85, 1.15),
+    ylims = (-0.15, 0.15),
+    xticks = [1.0],
+    yticks = [0.0],
+    xlabel="",
+    ylabel="",
+    xtickfont = Plots.font(10, "Computer Modern"), 
+    ytickfont = Plots.font(10, "Computer Modern"), 
+    aspect_ratio=:equal,
+    label="", 
+    lw=1.5,
+    grid=false,
+    linecolor=:red,
+    inset = Plots.bbox(0.01, 0.04, 0.24, 0.27, :top, :right),
+    subplot = 2,
+    framestyle=:box)
 
+Plots.scatter!(wm, 
+    xlabel="", 
+    ylabel="",
+    markercolor=:blue, 
+    markersize=3.5, 
+    label="", 
+    subplot=2)
 
-
+# Plots.savefig("polyvals_torus_CFIE_h_1.0m_cdt_0.1m.pdf")
 
 ```
     Plot the condition numbers respect to wave numbers
@@ -124,17 +157,17 @@ plt = Plots.plot(
     # xscale = :log10, 
     xlims = (4.17, 5.83),
     xticks = ([4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8], [4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8]),
-    xtickfont = font(9, "Computer Modern"), 
+    xtickfont = Plots.font(9, "Computer Modern"), 
     yscale = :log10, 
     ylims = (1, 1e5), 
     yticks = [1e0, 1e1, 1e2, 1e3, 1e4, 1e5],
-    ytickfont = font(10, "Computer Modern"),
+    ytickfont = Plots.font(10, "Computer Modern"),
     xlabel = "Wave number (1/m)",
     ylabel = "Condition number",
-    titlefont = font(10, "Computer Modern"), 
-    guidefont = font(11, "Computer Modern"),
-    colorbar_titlefont = font(10, "Computer Modern"), 
-    legendfont = font(10, "Computer Modern"))
+    titlefont = Plots.font(10, "Computer Modern"), 
+    guidefont = Plots.font(11, "Times"),
+    colorbar_titlefont = Plots.font(10, "Times"), 
+    legendfont = Plots.font(10, "Computer Modern"))
 
 Plots.plot!(cond_num[4:54,1], cond_num[4:54,2], label="EFIE", linecolor=:black, linestyle=:dash, markershape=:x, markercolor=:black, markersize=4.5)
 Plots.plot!(cond_num[4:54,1], cond_num[4:54,3], label="CFIE", linecolor=:black, linestyle=:dash, markershape=:circle, markercolor=:black, markersize=4)
@@ -146,31 +179,33 @@ Plots.savefig("cube_kappa.pdf")
     Plot the condition numbers with respect to mesh sizes
 ```
 
-cond_num = readdlm("sphere_cdt_1.txt", ' ', Float64, '\n')
+cond_num = DelimitedFiles.readdlm("sphere_cdt_1.txt", ' ', Float64, '\n')
 
 plt = Plots.plot(
     width = 600, height=400,
     grid = true,
     xscale = :log10, 
-    xlims = (0.06, 0.6),
-    xticks = ([0.06, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], [0.06, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]),
-    xtickfont = font(10, "Computer Modern"), 
+    xlims = (0.05, 0.6),
+    xticks = ([0.06, 0.1, 0.16, 0.2, 0.3, 0.5, 0.7, 1.0], [0.06, 0.1, 0.16, 0.2, 0.3, 0.5, 0.7, 1.0]),
+    xtickfont = Plots.font(10, "Computer Modern"), 
     yscale = :log10, 
-    ylims = (1e1, 1e4), 
-    yticks = [1e0, 1e1, 1e2, 1e3, 1e4],
-    ytickfont = font(10, "Computer Modern"),
+    ylims = (1e0, 1e4), 
+    yticks = [1e0, 1e1, 1e2, 1e3, 1e4, 1e5],
+    ytickfont = Plots.font(10, "Computer Modern"),
     xlabel = "Mesh size (m)",
     ylabel = "Condition number",
-    titlefont = font(10, "Computer Modern"), 
-    guidefont = font(11, "Computer Modern"),
-    colorbar_titlefont = font(10, "Computer Modern"), 
-    legendfont = font(10, "Computer Modern"))
+    titlefont = Plots.font(10, "Computer Modern"), 
+    guidefont = Plots.font(11, "Times"),
+    colorbar_titlefont = Plots.font(10, "Times"), 
+    legendfont = Plots.font(10, "Computer Modern"),
+    dpi = 300)
 
-Plots.plot!(cond_num[:,1], cond_num[:,2], label="", linecolor=1, lw=1.5, markershape=:square, markercolor=1, markersize=4)
-Plots.plot!(cond_num[:,1], cond_num[:,4], label="", linecolor=2, lw=1.5, markershape=:diamond, markercolor=2, markersize=5)
-Plots.plot!(cond_num[:,1], cond_num[:,6], label="mixed TD-CFIE", linecolor=3, markershape=:circle, markercolor=3, markersize=4)
-Plots.plot!(cond_num[:,1], cond_num[:,7], label="", linecolor=3, lw=1.5, markershape=:circle, markercolor=3, markersize=5)
-Plots.savefig("dense_discretization.pdf")
+Plots.plot!(cond_num[:,1], cond_num[:,2], label="TD-EFIE", linecolor=1, lw=1.6, markershape=:square, markercolor=1, markersize=4.2)
+Plots.plot!(cond_num[:,1], cond_num[:,3], label="TD-MFIE", linecolor=2, lw=1.6, markershape=:utriangle, markercolor=2, markersize=5)
+Plots.plot!(cond_num[:,1], cond_num[:,4], label="mixed TD-CFIE", linecolor=4, lw=1.6, markershape=:circle, markercolor=4, markersize=5)
+Plots.plot!(cond_num[:,1], cond_num[:,5], label="qHP TD-CFIE", linecolor=3, lw=1.6, markershape=:dtriangle, markercolor=3, markersize=5)
+
+Plots.savefig("cond_torus_cdt_8.pdf")
 
 
 
@@ -180,35 +215,34 @@ Plots.savefig("dense_discretization.pdf")
     Plot the condition numbers respect to time step size
 ```
 
-cond_num = readdlm("sphere_h_0.3m.txt", ' ', Float64, '\n')
+cond_num = DelimitedFiles.readdlm("sqtorus4holes_h_0.3m.txt", ' ', Float64, '\n')
 
 plt = Plots.plot(
     width = 600, height=400,
     grid = true,
     xscale = :log10, 
-    xlims = (0.6e0, 1e3),
+    xlims = (0.3e0, 1e4),
     xticks = [1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5],
-    xtickfont = font(10, "Computer Modern"), 
+    xtickfont = Plots.font(10, "Computer Modern"), 
     yscale = :log10, 
-    ylims = (1, 2e7), 
-    yticks = [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9],
-    ytickfont = font(10, "Computer Modern"),
+    ylims = (1, 1e9), 
+    yticks = [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11],
+    ytickfont = Plots.font(10, "Computer Modern"),
     xlabel = "Time step size (ns)",
     ylabel = "Condition number",
-    titlefont = font(10, "Computer Modern"), 
-    guidefont = font(11, "Computer Modern"),
-    colorbar_titlefont = font(10, "Computer Modern"), 
-    legendfont = font(11, "Computer Modern"),
-    legend = :topleft)
+    titlefont = Plots.font(10, "Computer Modern"), 
+    guidefont = Plots.font(11, "Times"),
+    colorbar_titlefont = Plots.font(10, "Times"), 
+    legendfont = Plots.font(11, "Computer Modern"),
+    legend = :topleft, 
+    dpi = 600)
 
-Plots.plot!(cond_num[2:12,1]/0.3, cond_num[2:12,2], label="", linecolor=1, lw=1.5, markershape=:square, markercolor=1, markersize=4)
-# Plots.plot!(cond_num[:,1], cond_num[:,3], label="qHP CP TD-EFIE", linecolor=2, markershape=:diamond, markercolor=2, markersize=4.5)
-Plots.plot!(cond_num[3:12,1]/0.3, cond_num[3:12,4], label="TD-MFIE", linecolor=2, lw=1.5, markershape=:diamond, markercolor=2, markersize=5)
-# Plots.plot!(cond_num[:,1], cond_num[:,5], label="qHP TD-MFIE", linecolor=4, markershape=:dtriangle, markercolor=4, markersize=4.5)
-Plots.plot!(cond_num[3:12,1]/0.3, cond_num[3:12,6], label="mixed TD-CFIE", linecolor=3, markershape=:circle, markercolor=3, markersize=4)
-Plots.plot!(cond_num[3:12,1]/0.3, cond_num[3:12,7], label="TD-CFIE", linecolor=3, lw=1.5, markershape=:circle, markercolor=3, markersize=5)
+Plots.plot!(cond_num[:,1]/0.3, cond_num[:,2], label="TD-EFIE", linecolor=1, lw=1.6, markershape=:square, markercolor=1, markersize=4.2)
+Plots.plot!(cond_num[:,1]/0.3, cond_num[:,3], label="TD-MFIE", linecolor=2, lw=1.6, markershape=:utriangle, markercolor=2, markersize=5)
+Plots.plot!(cond_num[:,1]/0.3, cond_num[:,4], label="mixed TD-CFIE", linecolor=4, lw=1.6, markershape=:circle, markercolor=4, markersize=5)
+Plots.plot!(cond_num[:,1]/0.3, cond_num[:,5], label="qHP TD-CFIE", linecolor=3, lw=1.6, markershape=:dtriangle, markercolor=3, markersize=5)
 
-Plots.savefig("large_time_step.pdf")
+Plots.savefig("cond_sphere_h_0.3m.pdf")
 
 
 
@@ -228,8 +262,8 @@ plt = Plots.plot(
     xlabel = "Observation angle",
     ylabel = "Scattered electric field",
     titlefont = font(10, "Computer Modern"), 
-    guidefont = font(11, "Computer Modern"),
-    colorbar_titlefont = font(10, "Computer Modern"), 
+    guidefont = font(11, "Times"),
+    colorbar_titlefont = font(10, "Times"), 
     legendfont = font(10, "Computer Modern"))
 
 Plots.plot(Θ1, norm.(Eexct), label="Mie series", linecolor=:black)
